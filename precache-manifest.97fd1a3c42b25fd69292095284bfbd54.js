1ca85c50f23ebcd6f092fb971778bf490072044f
rateb <geolocation@example.com>
2022-04-12 01:20:36 +0400

    Updates

---
+++ precache-manifest.97fd1a3c42b25fd69292095284bfbd54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aaf33beac5fa7da822de",
+    "url": "/Geo_Graph/static/css/main.77553440.chunk.css"
+  },
+  {
+    "revision": "aaf33beac5fa7da822de",
+    "url": "/Geo_Graph/static/js/main.aaf33bea.chunk.js"
+  },
+  {
+    "revision": "ffbe6cb2764bb680f2a9",
+    "url": "/Geo_Graph/static/js/1.ffbe6cb2.chunk.js"
+  },
+  {
+    "revision": "a8263b83a1b420e60c0e",
+    "url": "/Geo_Graph/static/js/runtime~main.a8263b83.js"
+  },
+  {
+    "revision": "05913471a7ec113f1108ac4656deeddd",
+    "url": "/Geo_Graph/index.html"
+  }
+];
